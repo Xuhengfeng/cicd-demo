a4293725804c50301db91797ee4bda27eaff72a0
xuhengfeng <xuhengfeng@zhixueyun.com>
2024-12-01 13:17:33 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <a href="https://vuejs.org/" target="_blank">
             <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
-        <h1>徐横峰聊前端</h1>
+        <h1>xhf聊前端</h1>
         <chartPie :series-data="dataList" :extra-option="extraOption" />
     </div>
     <!-- <HelloWorld msg="Vite + Vue" /> -->
